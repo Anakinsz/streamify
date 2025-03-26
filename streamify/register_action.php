80c06878ffcf23784ca72f8aad290e9453c386eb
Kin <160639020+K1nw@users.noreply.github.com>
2025-03-26 20:00:36 -0300

    register_action.php

--- streamify/register_action.php
+++ streamify/register_action.php
@@ -1,9 +1,9 @@
 <?php
-// Conexão com o banco de dados
+
 $servername = "localhost";
-$username = "root"; // O nome de usuário padrão no XAMPP é "root"
-$password = "";     // O MySQL geralmente não tem senha para o usuário root no XAMPP
-$dbname = "streamify";  // O nome do banco de dados que você acabou de criar
+$username = "root"; 
+$password = "";     
+$dbname = "streamify";  
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
@@ -12,7 +12,7 @@ if ($conn->connect_error) {
 }
 
 
-// Recebendo os dados do formulário
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $user = $_POST['username'];
     $email = $_POST['email'];
@@ -21,12 +21,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Hash da senha para maior segurança
     $password_hash = password_hash($password, PASSWORD_DEFAULT);
 
-    // SQL para inserir o novo usuário no banco de dados
+   
     $sql = "INSERT INTO users (username, email, password) VALUES ('$user', '$email', '$password_hash')";
 
     if ($conn->query($sql) === TRUE) {
         echo "Registro realizado com sucesso!";
-        // Aqui você pode redirecionar o usuário para a página de login ou para o painel
+        
         header("Location: login.php");
     } else {
         echo "Erro: " . $sql . "<br>" . $conn->error;
